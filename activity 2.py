db779359e8a8447f16f3f5203bcd2aacc5e6f314
Hoo Yu Heng <hooyuheng@gmail.com>
2017-11-16 17:50:28 +0800

    added weight saving function

--- activity 2.py
+++ activity 2.py
@@ -1,4 +1,21 @@
+import numpy as np
 
+temp = []
+weight = np.arange(10)
+print(weight)
 
-userInput = raw_input("Do you want to save the weights?(y/n): ")
-print(userInput)
+# userInput = input("Do you want to save the weights?(y/n): ")
+#
+# if userInput is "y" or "Y":
+#     with open("weights.txt", "w") as f:
+#         for w in weight:
+#             f.write(str(w) + "\n")
+#     print("Saved ! \n")
+
+userInput = input("Do you want to recall the saved weights?(y/n): ")
+if userInput is "y" or "Y":
+    with open("weights.txt", "r") as f:
+        for w in f:
+            temp.append(int(w.strip()))
+
+    print("Recalled: ", temp)
